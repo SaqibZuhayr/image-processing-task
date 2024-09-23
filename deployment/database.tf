006d16e5f04a8f617109387c3219940041912091
Saqib Zuhayr <saqibzuhair@gmail.com>
2024-09-24 01:15:50 +0500

    feature: add-image-processing-mechanism

--- deployment/database.tf
+++ deployment/database.tf
@@ -2,7 +2,7 @@ resource "aws_rds_cluster" "image_upload_aurora_postgres" {
   cluster_identifier      = "image-upload-aurora-postgres"
   engine                  = "aurora-postgresql"
   engine_version          = "15.4"
-  database_name           = "db"
+  database_name           = "image_upload_db"
   master_username         = "dbuser"
   master_password         = "dbpassword"
   skip_final_snapshot     = true
@@ -25,4 +25,19 @@ resource "aws_rds_cluster_instance" "image_upload_aurora_postgres_instance" {
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
   subnet_ids = module.vpc.private_subnets
+}
+
+# SQL schema for image upload
+resource "null_resource" "create_image_upload_schema" {
+  provisioner "local-exec" {
+    command = <<EOT
+      PGPASSWORD=${aws_rds_cluster.image_upload_aurora_postgres.master_password} psql \
+      -h ${aws_rds_cluster.image_upload_aurora_postgres.endpoint} \
+      -U ${aws_rds_cluster.image_upload_aurora_postgres.master_username} \
+      -d image_upload_db -c "
+      CREATE SCHEMA image_upload AUTHORIZATION ${aws_rds_cluster.image_upload_aurora_postgres.master_username};
+      "
+EOT
+  }
+  depends_on = [aws_rds_cluster_instance.image_upload_aurora_postgres_instance]
 }
